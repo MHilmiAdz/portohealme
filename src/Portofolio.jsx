5fec92cf761439d04dfdcf8c99832bf4f85f32af
Muhammad Hilmi Adzkia <103296966+MHilmiAdz@users.noreply.github.com>
2025-03-27 09:23:34 +0700

    add: adding Navbar, Experience and Certificate file jsx, and make all the component into 1 folder named "components"
    add: adding Navbar, Experience and Certificate file jsx, and make all the component into 1 folder named "components"

--- src/Portofolio.jsx
+++ src/Portofolio.jsx
@@ -1,12 +1,18 @@
-import { Footer } from "./Footer";
-import { Introduction } from "./Introduction";
-import { Projects } from "./Projects";
+import { Footer } from "./components/Footer";
+import { Introduction } from "./components/Introduction";
+import { Projects } from "./components/Projects";
+import { Certificate } from "./components/Certificate";
+import { Experience } from "./components/Experience";
+import { Navbar } from "./components/Navbar";
 
 export default function Portfolio() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
+      <Navbar />
       <Introduction />
       <Projects />
+      <Certificate />
+      <Experience />
       <Footer />
     </div>
   );
